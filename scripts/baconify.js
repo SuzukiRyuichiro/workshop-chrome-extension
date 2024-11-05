005c3fa887ece21c97ac3818052d447009795511
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-11-05 22:06:41 +0900

    Erase the testing code

--- scripts/baconify.js
+++ scripts/baconify.js
@@ -1,13 +1,5 @@
 const baconify = () => {
   // TODO: Add the image replacement script here
-  document.querySelectorAll("img").forEach((img) => {
-    img.src = `https://baconmockup.com/${img.width}/${img.height}`;
-    img.srcset = img.src;
-  });
 };
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "baconify") baconify();
-});
